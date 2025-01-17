df49bf1c46e2ba816d89e3222710a7294fe3d0d9
Fabian Affolter <fabian@affolter-engineering.ch>
2025-01-18 00:40:57 +0100

    Remove distutils

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ Licensed under MIT. All rights reserved.
 import os
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
